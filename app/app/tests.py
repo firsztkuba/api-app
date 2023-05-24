3123dc6428083adeb5f0002bbbd3d6b8b7cd33e9
Kuba Firszt <firszt.kuba@gmail.com>
2023-05-24 09:15:40 +0200

    first test

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers"""
+        res = calc.add(3,7)
+
+        self.assertEqual(res, 10)
