ff3cffce23561ac011085ec816de491194fefb39
Kuba Firszt <firszt.kuba@gmail.com>
2023-05-29 19:05:50 +0200

    Unit testing for database

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Commands to wait for database to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
+
