b67786a6c91cb2edd7fa33f49d8ec044c7f31732
Kuba Firszt <firszt.kuba@gmail.com>
2023-06-10 17:07:06 +0200

    Recipe models and tests

--- app/core/models.py
+++ app/core/models.py
@@ -17,9 +17,9 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("User must have an email address.")
-        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using = self.db)
+        user.save(using=self.db)
 
         return user
 
@@ -29,14 +29,14 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Users in the system"""
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
+    email = models.EmailField(max_length=255, unique = True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
